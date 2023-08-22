64989e613d5d24de1b738fc6df2248c378316ff3
Achraf Faris <95033881+achraffaris@users.noreply.github.com>
2023-08-22 15:19:27 +0000

    problem not fixed, changing AWS_DEFAULT_ACL to None

--- miniproject/miniproject/settings.py
+++ miniproject/miniproject/settings.py
@@ -153,7 +153,7 @@ AWS_S3_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_CUSTOM_DOMAIN=os.getenv('AWS_S3_CUSTOM_DOMAIN')
-AWS_DEFAULT_ACL='public-read'
+AWS_DEFAULT_ACL = None
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
